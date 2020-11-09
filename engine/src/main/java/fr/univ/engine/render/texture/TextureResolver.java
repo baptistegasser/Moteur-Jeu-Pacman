8c6d4fe1a46b0b955d258031d4448029954a3122
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-09 11:33:14 +0100

    Documented TextureResolver Interface

--- engine/src/main/java/fr/univ/engine/render/texture/TextureResolver.java
+++ engine/src/main/java/fr/univ/engine/render/texture/TextureResolver.java
@@ -2,6 +2,19 @@ package fr.univ.engine.render.texture;
 
 import javafx.scene.image.Image;
 
+/**
+ * A texture resolver is an object that is responsible of loading textures.
+ * Given the name of a texture, a texture loader should attempt to locate
+ * and load a texture.
+ * A texture is represented as a {@link javafx.scene.image.Image} instance.
+ */
 public interface TextureResolver {
-    Image getTexture(String name) throws IllegalArgumentException;
+    /**
+     * Locate and load a texture matching the given key.
+     *
+     * @param key the unique key identifying a texture
+     * @return a texture represented as a {@link javafx.scene.image.Image} instance
+     * @throws IllegalArgumentException if the method fail to load a texture for the given key
+     */
+    Image getTexture(String key) throws IllegalArgumentException;
 }
