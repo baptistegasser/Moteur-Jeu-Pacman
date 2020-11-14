85b886b75f4f0caa880aeb19e06742ba090317b7
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-14 19:03:53 +0100

    copy and compare points

--- engine/src/main/java/fr/univ/engine/math/Point.java
+++ engine/src/main/java/fr/univ/engine/math/Point.java
@@ -1,5 +1,7 @@
 package fr.univ.engine.math;
 
+import java.util.Objects;
+
 /**
  * Representation of a position in 2D space.
  */
@@ -10,5 +12,19 @@ public class Point {
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0;
     }
 }
