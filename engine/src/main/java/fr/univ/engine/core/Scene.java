7b503286c37ad908675907c6b84a77adc00b585f
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-14 16:00:17 +0100

    Added methods to get either the render or physic components of objects in a scene

--- engine/src/main/java/fr/univ/engine/core/Scene.java
+++ engine/src/main/java/fr/univ/engine/core/Scene.java
@@ -1,6 +1,11 @@
 package fr.univ.engine.core;
 
+import fr.univ.engine.physic.PhysicObject;
+import fr.univ.engine.render.RenderObject;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A scene representing a game's level.
@@ -12,11 +17,30 @@ public class Scene {
         this.objects = new ArrayList<>();
     }
 
-    public ArrayList<GameObject> getObjects() {
-        return objects;
-    }
-
+    /**
+     * Add an object to the scene.
+     *
+     * @param e the object to add
+     */
     public void add(GameObject e) {
         this.objects.add(e);
+    }
+
+    /**
+     * @return the list containing the physic component of each GameObject
+     */
+    public List<PhysicObject> getPhysicObjets() {
+        return objects.parallelStream()
+                .map(gameObject -> gameObject.physicObject)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @return the list containing the render component of each GameObject
+     */
+    public List<RenderObject> getRenderObjets() {
+        return objects.parallelStream()
+                .map(gameObject -> gameObject.renderObject)
+                .collect(Collectors.toList());
     }
 }
