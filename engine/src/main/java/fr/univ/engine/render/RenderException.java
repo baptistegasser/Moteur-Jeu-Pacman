31b84233561563acd048141beec95385335c71da
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-19 12:08:51 +0100

    Added constructor for exception without previous causes

--- engine/src/main/java/fr/univ/engine/render/RenderException.java
+++ engine/src/main/java/fr/univ/engine/render/RenderException.java
@@ -4,6 +4,10 @@ package fr.univ.engine.render;
  * Exceptions related to the render package usage.
  */
 class RenderException extends RuntimeException {
+    public RenderException(String message) {
+        super(message);
+    }
+
     public RenderException(String message, Throwable cause) {
         super(message, cause);
     }
