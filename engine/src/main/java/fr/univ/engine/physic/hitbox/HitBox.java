14882a27c4639f1db1cec8da02bf8adc517c3560
luc-bobo2 <luc.bobo@etu.univ-amu.fr>
2020-11-22 18:55:30 +0100

    commenting hitbox.java

--- engine/src/main/java/fr/univ/engine/physic/hitbox/HitBox.java
+++ engine/src/main/java/fr/univ/engine/physic/hitbox/HitBox.java
@@ -1,39 +1,58 @@
 package fr.univ.engine.physic.hitbox;
 
 import fr.univ.engine.math.Point;
-import javafx.scene.shape.Shape;
 
 /**
  * Represent the HitBox of object
  */
 public abstract class HitBox implements Intersect {
+
     /**
      * The position of the HitBox, set to be the center.
      */
     private Point position;
 
+    /**
+     * Specifiy if an object is solid
+     * An object can be solid such as a wall, or not, like a Gomme
+     * Pacman colide with solid objects.
+     */
     private boolean isSolid = false;
 
     public HitBox() {
         this.position = new Point(0, 0);
     }
 
+    /**
+     * @return The X position of the hitbox
+     */
     public double x() {
         return position.x;
     }
-
+    /**
+     * @return The Y position of the hitbox
+     */
     public double y() {
         return position.y;
     }
 
+    /**
+     * @param position Set the position of the hitbox
+     */
     public void setPosition(Point position) {
         this.position = position;
     }
 
+    /**
+     * @return The position of the hitbox
+     */
     public Point getPosition() {
         return position;
     }
 
+    /**
+     * @return If the position is solid
+     */
     public boolean isSolid() {
         return isSolid;
     }
