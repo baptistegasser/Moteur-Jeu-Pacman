7522d0ac805e181e984d1f3fd7e883966dc9e6df
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-22 13:12:20 +0100

    Added PhysicException

---
+++ engine/src/main/java/fr/univ/engine/physic/PhysicException.java
@@ -0,0 +1,9 @@
+package fr.univ.engine.physic;
+/**
+ * Exceptions related to the physic package usage.
+ */
+public class PhysicException extends RuntimeException {
+    public PhysicException(String message) {
+        super(message);
+    }
+}
