b89be25d4d4484e581229f3abdd3f4d8e412c3bd
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-25 18:40:09 +0100

    Add test for singleton logger

---
+++ engine/src/test/java/fr/univ/engine/logging/LoggerSingletonTest.java
@@ -0,0 +1,70 @@
+package fr.univ.engine.logging;
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.logging.Level;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for the {@link LoggerSingleton} class.
+ */
+class LoggerSingletonTest {
+    private final static PrintStream standardOut = System.out;
+    private final static ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private static LoggerSingleton logger;
+
+    /**
+     * Assert correct instanciation of singleton.
+     */
+    @BeforeAll
+    static void getInstance() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        assertNotNull(LoggerSingleton.getInstance());
+        logger = LoggerSingleton.getInstance();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.setOut(standardOut);
+    }
+
+    @BeforeEach
+    void setUp() {
+        outputStreamCaptor.reset();
+        logger.setAutoColor(false); // Assert default set to false
+    }
+
+    /**
+     * Assert that the {@link #()} method work.
+     */
+    @Test
+    void assertRedirectOutput() {
+        System.out.println("Test redirection");
+        assertEquals("Test redirection", outputStreamCaptor.toString().trim());
+    }
+
+    /**
+     * Assert that logger log.
+     */
+    @Test
+    void assertLogOutput() {
+        String expected = "[~? in fr.univ.engine.logging.LoggerSingletonTest#assertLogOutput()] - [INFO] - test message for logging";
+        logger.log(Level.INFO, "test message for logging");
+        assertEquals(expected, outputStreamCaptor.toString().trim());
+    }
+
+    /**
+     * Assert that the output is colored.
+     */
+    @Test
+    void setAutoColor() {
+        logger.setAutoColor(true);
+        String expected = "\u001b[38;2;222;49;99m[~? in fr.univ.engine.logging.LoggerSingletonTest#setAutoColor()] - [INFO] - test message for logging\u001b[0m\n";
+        logger.log(Level.INFO, "test message for logging");
+        assertEquals(expected, outputStreamCaptor.toString());
+    }
+}
