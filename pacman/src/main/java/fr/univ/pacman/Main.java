91691960d35962957d6db6f81515195f363ca1c8
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-11-25 18:57:34 +0100

    Add gamePlay in pacman

--- pacman/src/main/java/fr/univ/pacman/Main.java
+++ pacman/src/main/java/fr/univ/pacman/Main.java
@@ -1,8 +1,10 @@
 package fr.univ.pacman;
 
 import fr.univ.engine.core.Core;
+import fr.univ.engine.core.GameObject;
 import fr.univ.engine.render.WindowConfig;
 import fr.univ.engine.render.CachedResourcesLoader;
+import fr.univ.pacman.gameplay.GamePlay;
 import fr.univ.pacman.map.Map;
 
 /**
@@ -10,22 +12,6 @@ import fr.univ.pacman.map.Map;
  */
 public class Main {
     public static void main(String[] args) {
-        Core core = new Core(); // Initialize the engine
-
-        // Configure the windows
-        WindowConfig cfg = core.getRenderEngine().window;
-        cfg.width = 510;
-        cfg.height = 620;
-        cfg.title = "Pac-Man";
-        cfg.allowResize = false;
-        cfg.showFPSCounter = true;
-
-        // Create a resolver pointing to the assets dir inside the resources dir
-        CachedResourcesLoader resolver = new CachedResourcesLoader("assets/");
-        core.getRenderEngine().setTextureLoader(resolver);
-
-        core.setScene(new Map()); // Set the Pac-Man map
-
-        core.start(); // Start the game
+        GamePlay gamePlay = new GamePlay();
     }
 }
---
+++ pacman/src/main/java/fr/univ/pacman/gameplay/GamePlay.java
@@ -0,0 +1,31 @@
+package fr.univ.pacman.gameplay;
+
+import fr.univ.engine.core.Core;
+import fr.univ.engine.render.CachedResourcesLoader;
+import fr.univ.engine.render.WindowConfig;
+import fr.univ.pacman.map.Map;
+
+public class GamePlay {
+    Core core;
+    WindowConfig cfg;
+
+    public GamePlay() {
+        core = new Core(); // Initialize the engine
+
+        // Configure the windows
+        cfg = core.getRenderEngine().window;
+        cfg.width = 510;
+        cfg.height = 620;
+        cfg.title = "Pac-Man";
+        cfg.allowResize = false;
+        cfg.showFPSCounter = true;
+
+        // Create a resolver pointing to the assets dir inside the resources dir
+        CachedResourcesLoader resolver = new CachedResourcesLoader("assets/");
+        core.getRenderEngine().setTextureLoader(resolver);
+
+        core.setScene(new Map()); // Set the Pac-Man map
+
+        core.start(); // Start the game
+    }
+}
