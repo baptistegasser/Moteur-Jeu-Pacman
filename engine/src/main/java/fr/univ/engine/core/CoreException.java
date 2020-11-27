8a9421f1168656c13f40a1b038ad2878c861771e
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-27 20:00:57 +0100

    Added constructor to CoreException and JavaDoc

--- engine/src/main/java/fr/univ/engine/core/CoreException.java
+++ engine/src/main/java/fr/univ/engine/core/CoreException.java
@@ -4,6 +4,21 @@ package fr.univ.engine.core;
  * Exceptions related to the Core package usage.
  */
 public class CoreException extends RuntimeException {
+    /**
+     * Create an exception not based on a previous exception.
+     *
+     * @param message the message of the exception
+     */
+    public CoreException(String message) {
+        super(message);
+    }
+
+    /**
+     * Create an exception based on a previous exception.
+     *
+     * @param message the message of the exception
+     * @param cause the previous exception that triggered the creation of this exception
+     */
     public CoreException(String message, Throwable cause) {
         super(message, cause);
     }
