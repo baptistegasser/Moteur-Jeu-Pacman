ed353d848f9d2bb25c46f4aee419a823121a6b4b
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-27 15:13:09 +0100

    Added z-index attribute to texture

--- engine/src/main/java/fr/univ/engine/render/texture/Texture.java
+++ engine/src/main/java/fr/univ/engine/render/texture/Texture.java
@@ -8,9 +8,31 @@ import javafx.scene.image.ImageView;
  */
 public class Texture extends ImageView {
     /**
+     * Z-index used to tell if this texture should be behind or in front of another.
+     */
+    private int zIndex;
+
+    /**
      * Creates a new texture for the given image.
      */
-    Texture(Image image) {
+    public Texture(Image image) {
         super(image);
+        this.zIndex = 0;
+    }
+
+    /**
+     * Update the z-index position.
+     *
+     * @param zIndex the new z-index value
+     */
+    public void setZIndex(int zIndex) {
+        this.zIndex = zIndex;
+    }
+
+    /**
+     * @return the z-index value of this texture
+     */
+    public int zIndex() {
+        return zIndex;
     }
 }
