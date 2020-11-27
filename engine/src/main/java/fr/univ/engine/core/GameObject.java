4a5cd6e5fc8af0e85c15f3810ca42bda5535b626
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-27 16:15:43 +0100

    rought change to fit the RenderEntity interface

--- engine/src/main/java/fr/univ/engine/core/GameObject.java
+++ engine/src/main/java/fr/univ/engine/core/GameObject.java
@@ -4,7 +4,7 @@ import fr.univ.engine.math.Point;
 import fr.univ.engine.physic.PhysicEntity;
 import fr.univ.engine.physic.PhysicObject;
 import fr.univ.engine.render.RenderEntity;
-import fr.univ.engine.render.RenderObject;
+import fr.univ.engine.render.texture.Texture;
 
 import java.util.Objects;
 
@@ -22,11 +22,6 @@ public abstract class GameObject implements RenderEntity, PhysicEntity {
     /**
      * The render component used to display this object.
      */
-    protected RenderObject renderObject;
-
-    /**
-     * The render component used to display this object.
-     */
     protected PhysicObject physicObject;
 
     /**
@@ -34,22 +29,19 @@ public abstract class GameObject implements RenderEntity, PhysicEntity {
      */
     private String name;
 
+    private Point position;
+    private Texture texture;
+
     protected GameObject() {
         this(0, 0);
     }
 
     protected GameObject(double x, double y) {
-        Point pos = new Point(x, y);
+        this.position = new Point(x, y);
         this.ID = GameObject.currentIDs++;
-        this.physicObject = new PhysicObject(pos);
-        this.renderObject = new RenderObject(pos);
+        this.physicObject = new PhysicObject(this.position);
 
         setName("UNKNOWN GameObject"); // default GameObject name
-    }
-
-    @Override
-    public RenderObject getRenderObject() {
-        return renderObject;
     }
 
     @Override
@@ -64,7 +56,6 @@ public abstract class GameObject implements RenderEntity, PhysicEntity {
     public void setName(String name) {
         this.name = name;
         this.physicObject.setName(name);
-        this.renderObject.setName(name);
     }
 
     @Override
@@ -91,6 +82,19 @@ public abstract class GameObject implements RenderEntity, PhysicEntity {
     public void destroy() {
         Point pos = new Point(0, 0);
         this.physicObject = new PhysicObject(pos);
-        this.renderObject = new RenderObject(pos);
+    }
+
+    @Override
+    public Point pos() {
+        return this.position;
+    }
+
+    @Override
+    public Texture getTexture() {
+        return this.texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
     }
 }
