3de97ca04403e4532497e3056585dabd91ceb79d
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-27 16:09:01 +0100

    added check for null and clear and interface changes

--- engine/src/main/java/fr/univ/engine/render/renderer/JFXRenderer.java
+++ engine/src/main/java/fr/univ/engine/render/renderer/JFXRenderer.java
@@ -2,13 +2,14 @@ package fr.univ.engine.render.renderer;
 
 import fr.univ.engine.math.Point;
 import fr.univ.engine.render.RenderEntity;
+import fr.univ.engine.render.texture.Texture;
 import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * A renderer capable of rendering elements on a JavaFX {@link Canvas}.
@@ -21,21 +22,29 @@ public class JFXRenderer extends Renderer<Canvas> {
 
     @Override
     public void render(List<RenderEntity> entities) {
-        final List<Action> actions = new ArrayList<>();
+        final List<GraphicAction> actions = new ArrayList<>();
 
         for (RenderEntity entity : entities) {
-            double w = entity.getRenderObject().width;
-            double h = entity.getRenderObject().height;
-            Point pos = viewport.toAbsolutePos(entity.getPos(), w, h);
-            actions.add(ctx -> ctx.drawImage(entity.getTexture().getImage(), pos.x, pos.y, w, h));
+            Texture texture = entity.getTexture();
+            if (texture == null) continue;
+            Point pos = viewport.toAbsolutePos(entity.pos(), texture.width(), texture.height());
+            actions.add(ctx -> ctx.drawImage(texture.getImage(), pos.x, pos.y, texture.width(), texture.height()));
         }
 
         final GraphicsContext ctx = viewport.getArea().getGraphicsContext2D();
-        Platform.runLater(() -> actions.forEach(action -> action.apply(ctx)));
+        Platform.runLater(() -> {
+            ctx.setFill(Color.BLACK);
+            ctx.fillRect(0, 0, viewport.getWidth(), viewport.getHeight());
+            actions.forEach(action -> action.apply(ctx));
+        });
     }
 
+    /**
+     * Simple functional interface to store graphical change for later.
+     * Allow to batch multiple drawing actions.
+     */
     @FunctionalInterface
-    private interface Action {
+    private interface GraphicAction {
         void apply(GraphicsContext ctx);
     }
 }
