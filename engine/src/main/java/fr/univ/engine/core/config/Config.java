750459143d7407073b5548a131e7a33d42562e8e
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-28 12:34:14 +0100

    Added new way to configure a game

---
+++ engine/src/main/java/fr/univ/engine/core/config/Config.java
@@ -0,0 +1,31 @@
+package fr.univ.engine.core.config;
+
+/**
+ * The class charged of storing the config of a game.
+ */
+public final class Config {
+    /**
+     * The title of fix game, also the window title.
+     */
+    public String title = "Game Engine";
+    /**
+     * The version of this game.
+     */
+    public String version = "2.0";
+    /**
+     * The width of this window.
+     */
+    public double width = 1280;
+    /**
+     * The height of this window.
+     */
+    public double height = 720;
+    /**
+     * Should the fps be displayed in some way.
+     */
+    public boolean displayFPS = false;
+    /**
+     * Should the window be resizable.
+     */
+    public boolean resizable = true;
+}
