c397a1c08a3c4a19bfda1f5a1139df623a81fd01
Baptiste Gasser <baptistegasser36@gmail.com>
2020-11-29 17:22:20 +0100

    Fix TextureComparator breaking contract when t1 and t2 are null

--- engine/src/main/java/fr/univ/engine/render/texture/TextureComparator.java
+++ engine/src/main/java/fr/univ/engine/render/texture/TextureComparator.java
@@ -9,7 +9,9 @@ import java.util.Comparator;
 public class TextureComparator implements Comparator<Texture> {
     @Override
     public int compare(Texture t1, Texture t2) {
-        if (t1 == null) {
+        if (t1 == null && t2 == null) {
+            return 0;
+        } else if (t1 == null) {
             return -1;
         } else if (t2 == null) {
             return 1;
