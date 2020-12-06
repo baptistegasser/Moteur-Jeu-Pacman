a0c6a0e62737ccbe2cc7ded8b4005390393ad073
Baptiste Gasser <baptistegasser36@gmail.com>
2020-12-06 14:40:10 +0100

    update Point test

--- engine/src/test/java/fr/univ/engine/math/PointTest.java
+++ engine/src/test/java/fr/univ/engine/math/PointTest.java
@@ -1,6 +1,9 @@
 package fr.univ.engine.math;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
  * Tests for {@link Point} class.
  */
 class PointTest {
+    private Point point;
+
+    @BeforeEach
+    void setUp() {
+        point = new Point(1, 2);
+    }
+
+    @Test
+    void constructor() {
+        assertEquals(new Point(1, 2), new Point(BigDecimal.valueOf(1), BigDecimal.valueOf(2)));
+    }
 
     @Test
     void copy() {
@@ -42,5 +56,92 @@ class PointTest {
         assertFalse(a.equals(b) && b.equals(a));
         b.setY(1);
         assertTrue(a.equals(b) && b.equals(a));
+    }
+
+    @Test
+    void x() {
+        assertEquals(BigDecimal.valueOf(1.0), point.x());
+    }
+
+    @Test
+    void xValue() {
+        assertEquals(1, point.xValue());
+    }
+
+    @Test
+    void y() {
+        assertEquals(BigDecimal.valueOf(2.0), point.y());
+    }
+
+    @Test
+    void yValue() {
+        assertEquals(2, point.yValue());
+    }
+
+    @Test
+    void setX() {
+        point.setX(10);
+        assertEquals(10, point.xValue());
+    }
+
+    @Test
+    void setXBigDecimal() {
+        point.setX(BigDecimal.valueOf(10));
+        assertEquals(10, point.xValue());
+    }
+
+    @Test
+    void incrementX() {
+        point.incrementX();
+        assertEquals(2, point.xValue());
+    }
+
+    @Test
+    void incrementXDouble() {
+        point.incrementX(2);
+        assertEquals(3, point.xValue());
+    }
+
+    @Test
+    void incrementXBigDecimal() {
+        point.incrementX(BigDecimal.valueOf(2));
+        assertEquals(3, point.xValue());
+    }
+
+    @Test
+    void setY() {
+        point.setY(10);
+        assertEquals(10, point.yValue());
+    }
+
+    @Test
+    void setYBigDecimal() {
+        point.setY(BigDecimal.valueOf(10));
+        assertEquals(10, point.yValue());
+    }
+
+    @Test
+    void incrementY() {
+        point.incrementY();
+        assertEquals(3, point.yValue());
+    }
+
+    @Test
+    void incrementYDouble() {
+        point.incrementY(2);
+        assertEquals(4, point.yValue());
+    }
+
+    @Test
+    void incrementYBigDecimal() {
+        point.incrementY(BigDecimal.valueOf(2));
+        assertEquals(4, point.yValue());
+    }
+
+    @Test
+    void distance() {
+        Point a = new Point(0, 1);
+        Point b = new Point(10, 1);
+        assertEquals(10, a.distance(b));
     }
 }
