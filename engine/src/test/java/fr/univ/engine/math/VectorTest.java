1be7c9becdea4b24cbd39280acc10556a259e0e0
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-12-07 10:14:19 +0100

    Add getVectorForPasteEdge Tests

--- engine/src/test/java/fr/univ/engine/math/VectorTest.java
+++ engine/src/test/java/fr/univ/engine/math/VectorTest.java
@@ -110,5 +110,87 @@ class VectorTest {
 
     @Test
     void getVectorForPasteEdge() {
+        double speed1 = 1.0;
+
+        // Test without speed
+
+        Vector v1 = new Vector(0, 1);
+        Vector v2 = new Vector(0, 0.6);
+
+        assertEquals(v1.getVectorForPasteEdge(v2, speed1), new Vector(0,0.4));
+
+        Vector v3 = new Vector(-1, 10);
+        Vector v4 = new Vector(0.4, 2);
+
+        assertEquals(v3.getVectorForPasteEdge(v4, speed1), new Vector(-0.6,0));
+
+        Vector v5 = new Vector(-1, 0);
+        Vector v6 = new Vector(-1, 0);
+
+        assertNull(v5.getVectorForPasteEdge(v6, speed1));
+
+        Vector v7 = new Vector(-1, 0);
+        Vector v8 = new Vector(1, 0);
+
+        assertEquals(v7.getVectorForPasteEdge(v8, speed1), new Vector(0,0));
+
+        Vector v9 = new Vector(-1, 1);
+        Vector v10 = new Vector(-1, -1);
+
+        assertEquals(v9.getVectorForPasteEdge(v10, speed1), new Vector(0,0));
+
+        Vector v11 = new Vector(248.369, -921.3);
+        Vector v12 = new Vector(158.475, 48.29);
+
+        assertEquals(v11.getVectorForPasteEdge(v12, speed1), new Vector(89.894,0));
+
+        // Test with speed
+
+        Vector v13 = new Vector(0, 1);
+        Vector v14 = new Vector(0.5, 1);
+
+        assertEquals(v13.getVectorForPasteEdge(v14, speed1), new Vector(-0.5,1));
+
+        double speed2 = 0.7;
+
+        Vector v15 = new Vector(0.7, 0);
+        Vector v16 = new Vector(0.7, 0.4);
+
+        assertEquals(v15.getVectorForPasteEdge(v16, speed2), new Vector(0.7,-0.4));
+
+        Vector v17 = new Vector(0.7, 0);
+        Vector v18 = new Vector(0.7, 0.9);
+
+        assertNull(v17.getVectorForPasteEdge(v18, speed1));
+
+        Vector v19 = new Vector(0.7, 0);
+        Vector v20 = new Vector(0.7, -0.4);
+
+        assertEquals(v19.getVectorForPasteEdge(v20, speed2), new Vector(0.7,0.4));
+
+        Vector v21 = new Vector(0.7, 0);
+        Vector v22 = new Vector(0.7, -0.9);
+
+        assertNull(v21.getVectorForPasteEdge(v22, speed1));
+
+        Vector v23 = new Vector(0.7, 0);
+        Vector v24 = new Vector(0.8, 0.4);
+
+        assertEquals(v23.getVectorForPasteEdge(v24, speed2), new Vector(-0.1,0));
+
+        Vector v25 = new Vector(0.8, 0);
+        Vector v26 = new Vector(0, 0.4);
+
+        assertNull(v25.getVectorForPasteEdge(v26, speed1));
+
+        Vector v27 = new Vector(0.7, 0.4);
+        Vector v28 = new Vector(0.7, 0.4);
+
+        assertEquals(v27.getVectorForPasteEdge(v28, speed2), new Vector(0.7,-0.4));
+
+        Vector v29 = new Vector(0.7, 0.4);
+        Vector v30 = new Vector(0.7, -0.4);
+
+        assertEquals(v29.getVectorForPasteEdge(v30, speed2), new Vector(0,0));
     }
 }
